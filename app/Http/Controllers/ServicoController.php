c477406327e378062faaceb9e60398bdd686f3ab
Rogério <rogeriodorneas09@gmail.com>
2022-06-13 11:20:09 -0300

    Adcionado try catch e transaction no ServicoController

--- app/Http/Controllers/ServicoController.php
+++ app/Http/Controllers/ServicoController.php
@@ -4,14 +4,17 @@ namespace App\Http\Controllers;
 
 use App\Models\Produto;
 use App\Models\Servico;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Redirect;
 
 class ServicoController extends Controller
 {
     public function index()
     {
-        $produtos = Produto::all();
+        // $produtos = Produto::all();
+        $produtos = DB::table('produtos')->select('id', 'nome')->get();
         $servicos = Servico::all();
 
         return view('servicos', compact('produtos', 'servicos'));
@@ -19,21 +22,32 @@ class ServicoController extends Controller
 
     public function insert(Request $request)
     {
-        $validatedRequest = $request->validate([
-            'produto_id' => 'required',
-            'nome' => 'required|unique:servicos|max:255',
-            'descricao' => 'nullable'
-        ]);
+        try {
+            $validatedRequest = $request->validate([
+                'produto_id' => 'required',
+                'nome' => 'required|unique:servicos|max:255',
+                'descricao' => 'nullable'
+            ]);
 
-        $servico = Servico::create([
-            'nome' => $validatedRequest['nome'],
-            'descricao' => $validatedRequest['descricao'],
-        ]);
+            if ($request->produto_id == '') {
+                throw new Exception('');
+            }
 
-        $idProdutos = $validatedRequest['produto_id'];
+            DB::transaction(function () use($validatedRequest) {
+                $servico = Servico::create([
+                    'nome' => $validatedRequest['nome'],
+                    'descricao' => $validatedRequest['descricao'],
+                ]);
+        
+                $idProdutos = $validatedRequest['produto_id'];
+        
+                $servico->produtos()->sync($idProdutos);
+            });
+                        
+            return Redirect::route('servicos.index');
 
-        $servico->produtos()->sync($idProdutos);
-
-        return Redirect::route('servicos.index');
+        } catch (Exception $e) {
+            return response()->json(['mensage' => 'teste: '.$e->getMessage()]);
+        }
     }
 }
